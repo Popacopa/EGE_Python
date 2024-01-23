023abd362d8baf69d9c7ec7dfcb3272d84a404c5
pavel <paushakow@gmail.com>
2024-01-23 15:50:10 +0300

    18.01.2024 completed

--- 18.01.2024/ex24.py
+++ 18.01.2024/ex24.py
@@ -2,7 +2,7 @@ with open("C:/Users/paush/Desktop/24.txt") as file:
   file = file.read()
 
 file = file.replace('KL', '_')
-file = file.replace('LK', '_')
+file = file.replace('LK', '_') # не учел второй возможный порядок  
 file = file.split('_')
 file = list(map(lambda x: len(x) + 2, file))
 print(max(file))
