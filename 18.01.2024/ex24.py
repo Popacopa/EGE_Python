55d93a0b0f3bf1a71f4cf2641f7c18226d7031a4
pavel <paushakow@gmail.com>
2024-01-23 15:22:15 +0300

    18.01.2024 completed

---
+++ 18.01.2024/ex24.py
@@ -0,0 +1,8 @@
+with open("C:/Users/paush/Desktop/24.txt") as file:
+  file = file.read()
+
+file = file.replace('KL', '_')
+file = file.replace('LK', '_')
+file = file.split('_')
+file = list(map(lambda x: len(x) + 2, file))
+print(max(file))
