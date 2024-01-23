9f256b2acb3883bc62f3e960c98fa8b817efcaf5
pavel <paushakow@gmail.com>
2024-01-23 11:51:52 +0300

    18

--- 18.01.2024/ex18.py
+++ 18.01.2024/ex18.py
@@ -2,6 +2,15 @@ with open("C:/Users/paush/Desktop/18.txt") as file:
   file = [list(map(int, x[:-1].split('\t'))) for x in file]
 
 
-def move_max():...
+def move(s, x, y, matrix):
+  s += matrix[x][y]
+  if x == len(matrix) - 1 and y == len(matrix) - 1: return s
+  if x == len(matrix) - 1: return  move(s, x, y + 1, matrix)
+  if y == len(matrix) - 1: return  move(s, x + 1, y, matrix)
+  if x < len(matrix) - 1 and y < len(matrix) - 1: 
+    return min(move(s, x, y + 1, matrix), move(s, x + 1, y, matrix))
   
-def moev_min():...
+
+print(move(0, 0, 0, file))
+  
+
